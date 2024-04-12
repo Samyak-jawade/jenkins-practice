ce484d08be9cd8069b36855d2d5df39003ec577e
samyak <jawadesamyak@gmail.com>
2024-04-12 17:29:08 +0530

    --- Auto Git Commit ---

---
+++ studentapp_ui.groovy
@@ -0,0 +1,33 @@
+pipeline   {
+    agent any 
+        stages{
+            stage ('Pull'){
+                steps{
+                     git changelog: false, poll: false, url: 'https://github.com/Samyak-jawade/studentapp.ui.git'
+                     echo "pull done"
+                }
+            }
+            stage ('Build'){
+                steps{
+                    sh '/opt/apache-3.9.6/bin/mvn clean package'
+                     echo "build done"
+                }
+            }
+            stage ('Test'){
+                steps{
+                     echo "test done"
+                }
+            } 
+            stage ('Quality-Test'){
+                steps{
+                     echo "Quality-check done"
+                }
+            }
+            stage ('Depoly'){
+                steps{
+                     echo "depoly done"
+                }
+            }                                   
+        }
+    
+}
